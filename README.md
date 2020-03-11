f6be60a4443d27e15ee3b049d6182a35d3287229
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-03-11 19:15:58 -0400

    Updates README, adds files from database branch

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # stock-app
 
-This is an app to buy shares and see your balance for stocks.
+This is an app to buy shares and see your balance for stocks, and transactions you've made.
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const InitiateMongoServer = require("./routes/db");
+
+// Initiate Mongo Server
+InitiateMongoServer();
+
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+// Middleware
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API Successfully Connected"
+  });
+});
+
+
+app.listen(PORT, (req, res) => {
+  console.log(`Listening at port ${PORT}`);
+});
